6a6ffb3358712d3e640ff3108d0fa115e2fadf94
sudipto sarker <sudipto15-3271@diu.edu.bd>
2023-11-12 01:46:46 +0600

    uptade

---
+++ lib/screen/product_screen_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:crud_api/widgets/product_item.dart';
+
+class ProductScreenList extends StatelessWidget {
+  const ProductScreenList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Products List',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          ),
+          backgroundColor: Colors.orangeAccent,
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orangeAccent,
+            onPressed: (){
+
+        },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30)
+          ),
+          child: const Icon(Icons.add,size: 35,),
+        ),
+        body: ListView.separated(itemBuilder: (context, index) {
+          return const ProductItem();
+        },
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: 10,)
+    );
+  }
+}

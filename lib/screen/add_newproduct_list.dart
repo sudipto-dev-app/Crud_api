649d068ed20084a8f0cf000f2a91f0a83d04a7ab
sudipto sarker <sudipto15-3271@diu.edu.bd>
2023-11-12 01:45:58 +0600

    uptade

---
+++ lib/screen/add_newproduct_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewProductList extends StatefulWidget {
+  const AddNewProductList({super.key});
+
+  @override
+  State<AddNewProductList> createState() => _AddNewProductListState();
+}
+
+class _AddNewProductListState extends State<AddNewProductList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   ListTile(
+      onTap: (){
+        showDialog(context: context, builder: (context)=>const AlertDialog(
+          title: Text('Select Action'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title:Text('Edit'),
+                leading: Icon(Icons.edit),
+              ),
+              Divider(
+                height: 0,
+              ),
+              ListTile(
+                title: Text('Delete'),
+                leading: Icon(Icons.delete),
+              )
+            ],
+          ),
+        ));
+      },
+      title: const Text('Prodcut Title'),
+      subtitle: const Text('Product description'),
+      trailing: const Text('\$150',style: TextStyle(fontSize:16),),
+      leading: Image.network(
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrMLtsOqs3QfE9WMtU2tt9mT0pUHUdh3q_VelWI_plZqnyj5xgHVp1WR62n77lWxH405k&usqp=CAU',width: 80,),
+    );
+  }
+}
